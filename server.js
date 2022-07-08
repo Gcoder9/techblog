252e0a9a292b01068e1c5997437c0e9dc575fd5b
Gcoder9 <104526581+Gcoder9@users.noreply.github.com>
2022-07-08 09:14:06 -0400

    added schema.sql and connection.js

---
+++ server.js
@@ -0,0 +1,38 @@
+//require express for running as service
+const express = require("express");
+const session = require("express-session");
+//offload routes to controllers directory reference
+const routes = require("./controllers");
+
+const sequelize = require("./config/connection");
+//init session state
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+//setup port for local or heroku operation
+const PORT = process.env.PORT || 3001;
+
+//setup session object
+const sess = {
+  secret: "techBlog-secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+//point express to the session object
+app.use(session(sess));
+
+//enable POST handling in express
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
